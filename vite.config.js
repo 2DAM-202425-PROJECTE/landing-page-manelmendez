57fe5135b51a2d402a7520fe995a4593e6f7290c
manelmendez1 <manelmendez@iesebre.com>
2024-10-09 21:04:16 +0200

    Pàgina secundària per a Login definida al Vite Config JS

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,13 @@
-export default {
-// config options
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+export default defineConfig({
     base: '/landing-page-manelmendez/',
-}
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                nested: resolve(__dirname, 'login/login.html'),
+            },
+        },
+    },
+})
